1f686be94bdf46d2db17caab7ae8e9b2ad902e46
Pooja <109016215+Pooja-sahdevram@users.noreply.github.com>
2024-07-07 17:39:48 +0530

    RTK with todo

---
+++ React_/11_ReacttoolkitTodo/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+import "./App.css";
+import Addtodo from "./components/Addtodo";
+import Todos from "./components/Todos";
+
+function App() {
+  return (
+    <>
+      <Addtodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
---
+++ React_/11_ReacttoolkitTodo/vite-project/src/feature/todo/todoSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+const initialState = {
+  todos: [{ id: 1, text: "hello" }],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+
+    updatetodo: (state, action) => {
+      const { id, text } = action.payload;
+      const todo = state.todos.find((todo) => todo.id === id);
+      if (todo) {
+        if (todo) {
+          todo.text = text;
+        }
+      }
+    },
+  },
+});
+
+export const { addTodo, removeTodo, updatetodo } = todoSlice.actions;
+export default todoSlice.reducer;
