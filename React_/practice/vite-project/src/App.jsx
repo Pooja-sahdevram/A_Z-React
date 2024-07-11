a953a753facbcbe16631d45700874dfeee49bdf1
Pooja <109016215+Pooja-sahdevram@users.noreply.github.com>
2024-07-12 00:28:51 +0530

    theme

--- React_/practice/vite-project/src/App.jsx
+++ React_/practice/vite-project/src/App.jsx
@@ -2,12 +2,17 @@ import Currencyconvertor from "./components/Currencyconvertor";
 import Passwordgenerator from "./components/Passwordgenerator";
 import TICtactoe from "./components/TICtactoe";
 import Weather from "./components/Weather";
+import { Provider } from "react-redux";
+import { store } from "./components/themechanger/storetheme/store";
+import Todos from "./components/rtkcompoenents/Todos";
+import Form from "./components/rtkcompoenents/Form";
+import Themechanger from "./components/themechanger/Themechanger";
 
 function App() {
   return (
-    <>
-      <TICtactoe />{" "}
-    </>
+    <Provider store={store}>
+      <Themechanger />
+    </Provider>
   );
 }
 
