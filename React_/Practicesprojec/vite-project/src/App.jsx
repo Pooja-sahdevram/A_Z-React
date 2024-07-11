b93ec50625a83b00c8fbfb19b6e64a6dcf9f40ea
Pooja <109016215+Pooja-sahdevram@users.noreply.github.com>
2024-07-11 20:13:47 +0530

    updated commit

---
+++ React_/Practicesprojec/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Currencyconvertor from "./components/Currencyconvertor";
+import Passwordgenerator from "./components/Passwordgenerator";
+import TICtactoe from "./components/TICtactoe";
+import Weather from "./components/Weather";
+
+function App() {
+  return (
+    <>
+      <TICtactoe />{" "}
+    </>
+  );
+}
+
+export default App;
